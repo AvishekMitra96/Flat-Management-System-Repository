8cedcddf43cf65cc9568d0579ff38056012c7076
Avishek <mitra9691@outlook.com>
2026-01-18 23:33:39 +0530

    Adding project Flat Management System

---
+++ Source Code/Java Code/fmsapp/src/main/java/com/fms/fmsapp/FlatManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fms.fmsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlatManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlatManagementSystemApplication.class, args);
+	}
+
+}
